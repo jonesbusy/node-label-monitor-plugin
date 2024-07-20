df77fd3a8217c17bf40310541fbd7606996fed73
Valentin Delaye <jonesbusy@gmail.com>
2024-07-20 12:54:15 +0200

    Add platform labeler for test

--- src/main/java/io/jenkins/plugins/nodelabelmonitor/ForbiddenLabelListener.java
+++ src/main/java/io/jenkins/plugins/nodelabelmonitor/ForbiddenLabelListener.java
@@ -2,14 +2,11 @@ package io.jenkins.plugins.nodelabelmonitor;
 
 import hudson.Extension;
 import hudson.FilePath;
-import hudson.model.AdministrativeMonitor;
 import hudson.model.Computer;
 import hudson.model.TaskListener;
 import hudson.node_monitors.NodeMonitor;
 import hudson.remoting.Channel;
 import hudson.slaves.ComputerListener;
-import jenkins.model.Jenkins;
-
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,7 +17,8 @@ public class ForbiddenLabelListener extends ComputerListener {
     private static final Logger LOGGER = Logger.getLogger(ForbiddenLabelListener.class.getName());
 
     @Override
-    public void preOnline(Computer c, Channel channel, FilePath root, TaskListener listener) throws IOException, InterruptedException {
+    public void preOnline(Computer c, Channel channel, FilePath root, TaskListener listener)
+            throws IOException, InterruptedException {
         refreshMonitor();
         LOGGER.log(Level.FINE, "preOnline() for forbidden label. Monitor refreshed");
     }
@@ -32,17 +30,15 @@ public class ForbiddenLabelListener extends ComputerListener {
     }
 
     private void refreshMonitor() {
-        NodeMonitor.getAll().stream().filter(nm -> nm instanceof ForbiddenLabelMonitor).forEach(
-                nm -> {
+        NodeMonitor.getAll().stream()
+                .filter(nm -> nm instanceof ForbiddenLabelMonitor)
+                .forEach(nm -> {
                     try {
                         Thread thread = nm.triggerUpdate();
                         thread.join();
-                    }
-                    catch (InterruptedException e) {
+                    } catch (InterruptedException e) {
                         LOGGER.log(Level.WARNING, "Error refreshing monitor", e);
                     }
-                }
-        );
+                });
     }
-
 }
